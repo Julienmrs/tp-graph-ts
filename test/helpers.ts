7281a22331e116e38da3764410dfb96208cbb784
julienmrs <jlpmrs@gmail.com>
2025-11-14 16:31:16 +0100

    0.2 - Ajout de fabriques pour les sommets et les arcs

--- test/helpers.ts
+++ test/helpers.ts
@@ -1,6 +1,7 @@
 import { Edge } from "../src/model/Edge";
 import { Graph } from "../src/model/Graph";
 import { Vertex } from "../src/model/Vertex";
+import Coordinate from '../src/model/Coordinate';
 
 /**
  * Create a sample graph
@@ -8,37 +9,13 @@ import { Vertex } from "../src/model/Vertex";
 export function createTestGraph01(): Graph {
 	const g = new Graph();
 
-	const a = new Vertex();
-	a.id = "a";
-	a.coordinate = [0.0, 0.0];
-	g.vertices.push(a);
-
-	const b = new Vertex();
-	b.id = "b";
-	b.coordinate = [1.0, 0.0];
-	g.vertices.push(b);
-
-	const c = new Vertex();
-	c.id = "c";
-	c.coordinate = [2.0, 0.0];
-	g.vertices.push(c);
-
-	const d = new Vertex();
-	d.id = "d";
-	d.coordinate = [1.0, 1.0];
-	g.vertices.push(d);
-
-	const ab = new Edge(a,b);
-	ab.id = "ab";
-	g.edges.push(ab);
-
-	const bc = new Edge(b,c);
-	bc.id = "bc";
-	g.edges.push(bc);
-
-	const ad = new Edge(a,d);
-	ad.id = "ad";
-	g.edges.push(ad);
+	const a = g.createVertex([0.0, 0.0] ,"a");
+	const b = g.createVertex([1.0, 0.0],"b");
+	const c = g.createVertex([2.0, 0.0],"c");
+	const d = g.createVertex([1.0, 1.0],"d");
+	g.createEdge(a,b,"ab");
+	g.createEdge(a,d,"ad");
+	g.createEdge(b,c,"bc");
 
 	return g;
 }
